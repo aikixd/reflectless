5a64a98a7553c8860d69c35a4c90c05876ebb769
aikixd <aikixd@gmail.com>
2019-07-23 17:18:47 +0300

    Function parameters macro

--- src/fn_services.rs
+++ src/fn_services.rs
@@ -10,6 +10,21 @@ pub struct FnBinding<'ctx, TFn, TCtx, TParam, TResult>
     phantom_result: PhantomData<TResult>
 }
 
+pub trait FnContext<T>
+{
+    fn extract(&self) -> T;
+}
+
+pub trait Binder<'ctx, TCtx, TParam>
+{
+    fn make_params(&self, ctx: &'ctx TCtx) -> TParam;
+}
+
+pub trait TupleArgs<TParam, TResult>
+{
+    fn call(&self, params: TParam) -> TResult;
+}
+
 impl<'ctx, TFn, TCtx, TParam, TResult> FnBinding<'ctx, TFn, TCtx, TParam, TResult>
     where 
         TFn : Fn(TParam) -> TResult + Binder<'ctx, TCtx, TParam>
@@ -36,16 +51,6 @@ impl<'ctx, TFn, TCtx, TParam, TResult> FnBinding<'ctx, TFn, TCtx, TParam, TResul
     }
 }
 
-pub trait FnContext<T>
-{
-    fn extract(&self) -> T;
-}
-
-pub trait Binder<'ctx, TCtx, TParam>
-{
-    fn make_params(&self, ctx: &'ctx TCtx) -> TParam;
-}
-
 macro_rules! binder_impl {
     ( $head:ident $( $tail:ident )* ) => {
         impl<'ctx, TCtx, Func, TResult, $head, $( $tail ),* > Binder<'ctx, TCtx, ( $head, $( $tail),* )> for Func
@@ -69,3 +74,40 @@ macro_rules! binder_impl {
 
 binder_impl!(T1 T2 T3 T4 T5 T6 T7 T8);
 
+macro_rules! tuple_args_impl {
+    ( $( ( $type:ident $name:ident) )* ) => {
+        impl<Func, $( $type, )* TResult> TupleArgs<($( $type, )*), TResult> for Func
+            where Func : Fn($( $type, )*) -> TResult
+        {
+            fn call(&self, ( $( $name, )* ) : ( $( $type, )* )) -> TResult
+            {
+                self($( $name, )*)
+            }
+        }
+    };
+
+    () => {};
+}
+
+macro_rules! tuple_args {
+    ( ( $h_type:ident $h_name:ident ) $( ( $t_type:ident $t_name:ident ) )* ) => {
+        tuple_args_impl!(( $h_type $h_name ) $( ( $t_type $t_name ) )* );
+        tuple_args!( $( ( $t_type $t_name ) )* );
+    };
+
+    () => {};
+}
+
+tuple_args!((T1 t1) (T2 t2) (T3 t3) (T4 t4) (T5 t5) (T6 t6) (T7 t7) (T8 t8));
+/*
+impl<Func, T1, T2, TResult> TupleArgs<(T1, T2,), TResult> for Func
+    where Func : Fn(T1, T2) -> TResult
+{
+    fn call(&self, params: (T1, T2)) -> TResult
+    {
+        self(params.0, params.1)
+    }
+}
+
+
+*/
